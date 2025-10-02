7adb4b4fec19465e71193c3ca9641b1be20513d1
Jan Albrecht <j_albr16@uni-muenster.de>
2025-10-02 11:00:12 +0200

    lualine + todo comment

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -3,6 +3,27 @@ return {
 	"echasnovski/mini.icons",
 	"tanvirtin/monokai.nvim",
 	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+        config = function ()
+            require('todo-comments').setup()
+            vim.keymap.set('n', '<leader>ft',  ':TodoTelescope<cr>')
+            vim.keymap.set('n', '<leader>xt',  ':Trouble todo<cr>')
+        end
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{
 		"terrortylor/nvim-comment",
 		config = function()
 			require("nvim_comment").setup()
